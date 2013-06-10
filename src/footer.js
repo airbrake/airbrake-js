6d2733036684426e6f27d84ad043c011cdef25a6
Duncan Beevers <duncan@dweebd.com>
2013-06-10 18:52:39 -0500

    Browser/Node-compatible Global

--- src/footer.js
+++ src/footer.js
@@ -1,1 +1,1 @@
-})(window, document, airbrake_client_app_protocol, airbrake_client_app_location, airbrake_client_app_hostname, airbrake_client_app_hash, airbrake_client_app_navigator_user_agent, airbrake_client_app_href, airbrake_client_app_create_xml_http_request);
+})(Global, Global.document, airbrake_client_app_protocol, airbrake_client_app_location, airbrake_client_app_hostname, airbrake_client_app_hash, airbrake_client_app_navigator_user_agent, airbrake_client_app_href, airbrake_client_app_create_xml_http_request);
--- src/header.js
+++ src/header.js
@@ -12,14 +12,20 @@ var make_mock_xml_http_request = function(url, data) {
   console.log(data);
 };
 
-var airbrake_client_app_protocol = (window.location ? window.location.protocol : '[app_protocol]'),
-    airbrake_client_app_location = (window.location ? window.location.protocol + '//' + window.location.host : '[app_url]'),
-    airbrake_client_app_hostname = (window.location ? window.location.hostname : '[app_hostname]'),
-    airbrake_client_app_hash = (window.location ? window.location.hash : '[app_hash]'),
-    airbrake_client_app_navigator_user_agent = (window.navigator ? window.navigator.userAgent : '[app_navigator_user_agent]'),
-    airbrake_client_app_href = (document.location ? document.location.href : '[app_doc_location_href]'),
-    airbrake_client_app_create_xml_http_request = (window.XMLHttpRequest ? make_xml_http_request : make_mock_xml_http_request);
+var Global;
+if ("undefined" === typeof GLOBAL) {
+  Global = this;
+} else {
+  Global = GLOBAL;
+}
 
+var airbrake_client_app_protocol = (Global.location ? Global.location.protocol : '[app_protocol]'),
+    airbrake_client_app_location = (Global.location ? Global.location.protocol + '//' + Global.location.host : '[app_url]'),
+    airbrake_client_app_hostname = (Global.location ? Global.location.hostname : '[app_hostname]'),
+    airbrake_client_app_hash = (Global.location ? Global.location.hash : '[app_hash]'),
+    airbrake_client_app_navigator_user_agent = (Global.navigator ? Global.navigator.userAgent : '[app_navigator_user_agent]'),
+    airbrake_client_app_href = (Global.document ? Global.document.location.href : '[app_doc_location_href]'),
+    airbrake_client_app_create_xml_http_request = (Global.XMLHttpRequest ? make_xml_http_request : make_mock_xml_http_request);
 
 // Airbrake JavaScript Notifier Bundle
 (function(window, document, airbrake_client_app_protocol, airbrake_client_app_location, airbrake_client_app_hostname, airbrake_client_app_hash, airbrake_client_app_navigator_user_agent, airbrake_client_app_href, airbrake_client_app_create_xml_http_request, undefined) {
