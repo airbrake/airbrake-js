7d2979b000eb369791c90877c5006b616145ff54
Duncan Beevers <duncan@dweebd.com>
2013-06-10 20:45:33 -0500

    2-space tabs

--- tests/spec/performanceSpecs.js
+++ tests/spec/performanceSpecs.js
@@ -1,62 +1,62 @@
 describe("Performance testing.", function () {
-    var _i = 0,
-        _XMLHttpRequest = window.XMLHttpRequest,
-        _callsNumber = 1000,
-        _recCallsLevel = 10000;
+  var _i = 0,
+    _XMLHttpRequest = window.XMLHttpRequest,
+    _callsNumber = 1000,
+    _recCallsLevel = 10000;
 
-    window.Airbrake.setOutputFormat('JSON');
+  window.Airbrake.setOutputFormat('JSON');
 
-    beforeEach(function() {
-        spyOn(window.XMLHttpRequest.prototype, 'open').andCallFake(function() {
+  beforeEach(function() {
+    spyOn(window.XMLHttpRequest.prototype, 'open').andCallFake(function() {
 
-        });
-
-        spyOn(window.XMLHttpRequest.prototype, 'setRequestHeader').andCallFake(function() {
-
-        });
-
-        spyOn(window.XMLHttpRequest.prototype, 'send').andCallFake(function() {
-
-        });
     });
 
-    it(_callsNumber + " exceptions", function () {
-        for (_i = 0; _i < _callsNumber; _i += 1) {
-            try {
-                (0)();
-            } catch (e) {
-                window.Airbrake.captureException(e);
-            }
-        }
+    spyOn(window.XMLHttpRequest.prototype, 'setRequestHeader').andCallFake(function() {
 
-        expect(window.XMLHttpRequest.prototype.open).toHaveBeenCalled();
-        expect(window.XMLHttpRequest.prototype.send).toHaveBeenCalled();
-
-        expect(window.XMLHttpRequest.prototype.open.calls.length).toEqual(_callsNumber);
-        expect(window.XMLHttpRequest.prototype.send.calls.length).toEqual(_callsNumber);
     });
 
-    it(_callsNumber + " deeply nested exception", function () {
-        var _funcRec = function (level) {
-            if (level === 0) {
-                (0)();
-            } else {
-                _funcRec(level - 1);
-            }
-        };
+    spyOn(window.XMLHttpRequest.prototype, 'send').andCallFake(function() {
 
-        for (_i = 0; _i < _callsNumber; _i += 1) {
-            try {
-                _funcRec(_recCallsLevel);
-            } catch (e) {
-                window.Airbrake.captureException(e);
-            }
-        }
-
-        expect(window.XMLHttpRequest.prototype.open).toHaveBeenCalled();
-        expect(window.XMLHttpRequest.prototype.send).toHaveBeenCalled();
-
-        expect(window.XMLHttpRequest.prototype.open.calls.length).toEqual(_callsNumber);
-        expect(window.XMLHttpRequest.prototype.send.calls.length).toEqual(_callsNumber);
     });
+  });
+
+  it(_callsNumber + " exceptions", function () {
+    for (_i = 0; _i < _callsNumber; _i += 1) {
+      try {
+        (0)();
+      } catch (e) {
+        window.Airbrake.captureException(e);
+      }
+    }
+
+    expect(window.XMLHttpRequest.prototype.open).toHaveBeenCalled();
+    expect(window.XMLHttpRequest.prototype.send).toHaveBeenCalled();
+
+    expect(window.XMLHttpRequest.prototype.open.calls.length).toEqual(_callsNumber);
+    expect(window.XMLHttpRequest.prototype.send.calls.length).toEqual(_callsNumber);
+  });
+
+  it(_callsNumber + " deeply nested exception", function () {
+    var _funcRec = function (level) {
+      if (level === 0) {
+        (0)();
+      } else {
+        _funcRec(level - 1);
+      }
+    };
+
+    for (_i = 0; _i < _callsNumber; _i += 1) {
+      try {
+        _funcRec(_recCallsLevel);
+      } catch (e) {
+        window.Airbrake.captureException(e);
+      }
+    }
+
+    expect(window.XMLHttpRequest.prototype.open).toHaveBeenCalled();
+    expect(window.XMLHttpRequest.prototype.send).toHaveBeenCalled();
+
+    expect(window.XMLHttpRequest.prototype.open.calls.length).toEqual(_callsNumber);
+    expect(window.XMLHttpRequest.prototype.send.calls.length).toEqual(_callsNumber);
+  });
 });
