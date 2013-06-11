5cdc4dbb1920ed0124981f037ee6d99ae15511d0
Duncan Beevers <duncan@dweebd.com>
2013-06-11 11:24:17 -0500

    Comment about where notifier comes from

--- src/util/window_onerror.js
+++ src/util/window_onerror.js
@@ -1,4 +1,6 @@
-var Notifier = require("../notifier");
+// We have to rely on the magic locals provided by header.js right now
+// Once we're decoupled we'll be able to require notifier directly
+var Notifier = require("../concat-dist");
 
 window.onerror = function (message, file, line) {
   setTimeout(function () {
