4d64aa7996f45cfd8f3fa65fbddf57b718bc2db3
Duncan Beevers <duncan@dweebd.com>
2013-06-11 13:01:46 -0500

    Explicit instance reference

--- src/Client.js
+++ src/Client.js
@@ -2,38 +2,45 @@
 // It stores configuration information and dispatches errors.
 // window.Airbrake is an instance of Client.
 function Client(processor, reporter, setupjQueryTracker) {
+  var instance = this;
+
   var _environment;
-  this.setEnvironment = function(val) { _environment = val; };
-  this.getEnvironment = function() { return _environment; };
+  instance.setEnvironment = function(val) { _environment = val; };
+  instance.getEnvironment = function() { return _environment; };
 
   var _key;
-  this.setKey = function(val) { _key = val; };
-  this.getKey = function() { return _key; };
+  instance.setKey = function(val) { _key = val; };
+  instance.getKey = function() { return _key; };
 
   var _host;
-  this.setHost = function(val) { _host = val; };
-  this.getHost = function() { return _host; };
+  instance.setHost = function(val) { _host = val; };
+  instance.getHost = function() { return _host; };
 
   var _errorDefaults;
-  this.setErrorDefaults = function(val) { _errorDefaults = val; };
-  this.getErrorDefaults = function() { return _errorDefaults; };
+  instance.setErrorDefaults = function(val) { _errorDefaults = val; };
+  instance.getErrorDefaults = function() { return _errorDefaults; };
 
   var _guessFunctionName = false;
-  this.setGuessFunctionName = function(val) { _guessFunctionName = val; };
-  this.getGuessFunctionName = function() { return _guessFunctionName; };
+  instance.setGuessFunctionName = function(val) { _guessFunctionName = val; };
+  instance.getGuessFunctionName = function() { return _guessFunctionName; };
 
   var _outputFormat = "JSON";
-  this.setOutputFormat = function(val) { _outputFormat = val; };
-  this.getOutputFormat = function() { return _outputFormat; };
+  instance.setOutputFormat = function(val) { _outputFormat = val; };
+  instance.getOutputFormat = function() { return _outputFormat; };
 
   var _trackJQ = false;
-  this.setTrackJQ = function(val) {
+  instance.setTrackJQ = function(val) {
     if (!_trackJQ && val && setupjQueryTracker) {
       setupjQueryTracker();
     }
     _trackJQ = val;
   };
-  this.getTrackJQ = function() { return !!_trackJQ; };
+  instance.getTrackJQ = function() { return !!_trackJQ; };
+
+  instance.captureException = function(exception) {
+    var data = processor.process(exception);
+    reporter.report(data);
+  };
 }
 
 module.exports = Client;
