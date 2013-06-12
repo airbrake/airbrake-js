8d70c4fe8d665da14edb866e29feeb361840cc94
Duncan Beevers <duncan@dweebd.com>
2013-06-12 16:58:43 -0500

    Install bower and tracekit component

--- cookbooks/npm_package_json/recipes/default.rb
+++ cookbooks/npm_package_json/recipes/default.rb
@@ -1,3 +1,3 @@
 execute "npm install" do
-  command "su -c \"cd /vagrant; npm install --save-dev\" vagrant"
+  command "su -c \"cd /vagrant; npm install --save-dev; ./node_modules/.bin/bower install\" vagrant"
 end
