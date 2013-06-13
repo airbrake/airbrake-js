53beef54497b0049dd7b0dc6aa0234583f1096a2
Duncan Beevers <duncan@dweebd.com>
2013-06-13 01:18:25 -0500

    Disable window.onerror

--- src/util/window_onerror.js
+++ src/util/window_onerror.js
@@ -1,15 +1,15 @@
-// We have to rely on the magic locals provided by header.js right now
-// Once we're decoupled we'll be able to require notifier directly
-var Notifier = require("../concat-dist");
+// // We have to rely on the magic locals provided by header.js right now
+// // Once we're decoupled we'll be able to require notifier directly
+// var Notifier = require("../concat-dist");
 
-// TODO: Drive this from somewhere rather than having it happen automatically
-global.onerror = function (message, file, line) {
-  setTimeout(function () {
-    new Notifier().notify({
-      message: message,
-      stack: '()@' + file + ':' + line
-    });
-  }, 0);
+// // TODO: Drive this from somewhere rather than having it happen automatically
+// global.onerror = function (message, file, line) {
+//   setTimeout(function () {
+//     new Notifier().notify({
+//       message: message,
+//       stack: '()@' + file + ':' + line
+//     });
+//   }, 0);
 
-  return true;
-};
+//   return true;
+// };
