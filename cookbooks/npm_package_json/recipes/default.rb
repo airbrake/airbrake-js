d5eaf90f007f70cf5b8476bc441e085f8d98c3df
Duncan Beevers <duncan@dweebd.com>
2013-06-13 19:53:53 -0500

    Split npm and bower install commands

--- cookbooks/npm_package_json/recipes/default.rb
+++ cookbooks/npm_package_json/recipes/default.rb
@@ -1,3 +1,7 @@
 execute "npm install" do
-  command "su -c \"cd /vagrant; npm install --save-dev; ./node_modules/.bin/bower install\" vagrant"
+  command "su -c \"cd /vagrant; npm install --save-dev\" vagrant"
+end
+
+execute "bower install" do
+  command "su -c \"cd /vagrant; ./node_modules/.bin/bower install\" vagrant"
 end
