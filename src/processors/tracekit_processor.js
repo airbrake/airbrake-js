7e29585e32ddffab13cf65c5070a3387e2363082
Duncan Beevers <duncan@dweebd.com>
2013-07-11 15:33:16 -0700

    TraceKit processor relies on #report

--- src/processors/tracekit_processor.js
+++ src/processors/tracekit_processor.js
@@ -1,27 +1,31 @@
 var TraceKit = require("../shims/tracekit_browserify_shim");
-TraceKit.remoteFetching = false;
-TraceKit.collectWindowErrors = false;
+TraceKit.remoteFetching = true;
+TraceKit.collectWindowErrors = true;
 
-function TraceKitProcessor() {
-  this.process = function(error, fn) {
-    var tracekit_result = TraceKit.computeStackTrace(error),
-        stack = tracekit_result.stack;
+TraceKit.report.subscribe(function(tracekit_result, fn) {
+  var stack = tracekit_result.stack;
 
-    var backtrace = [], i, frame;
-    for (i = stack.length - 1; i >= 0; i--) {
-      frame = stack[i];
-      backtrace.unshift({
-        file: frame.url,
-        line: parseInt(frame.line, 10),
-        "function": frame.func
-      });
-    }
+  var backtrace = [], i, frame;
+  for (i = stack.length - 1; i >= 0; i--) {
+    frame = stack[i];
+    backtrace.unshift({
+      file: frame.url,
+      line: parseInt(frame.line, 10),
+      "function": frame.func
+    });
+  }
 
     fn({
       type: tracekit_result.name,
       message: tracekit_result.message,
       backtrace: backtrace
     });
+
+});
+
+function TraceKitProcessor() {
+  this.process = function(error, fn) {
+    TraceKit.report(error, fn);
   };
 }
 
