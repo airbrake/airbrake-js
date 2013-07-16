50a90f449655f4e583d4464350444c2b4f874b62
Duncan Beevers <duncan@dweebd.com>
2013-07-16 10:16:07 -0700

    Use getProject for project_id and key

--- src/main.js
+++ src/main.js
@@ -13,9 +13,10 @@ function getReporter(client) {
   var protocol = ("https:" === global.location.protocol ? "https://" : "http://");
 
   // Vars from client
-  var host        = "api.airbrake.io",
-      project_id  = client.getProjectId(),
-      key         = client.getKey(),
+  var host               = "api.airbrake.io",
+      project_id_and_key = client.getProject(),
+      project_id         = project_id_and_key[0],
+      key                = project_id_and_key[1],
       environment_name   = client.getEnvironmentName();
 
   var url = protocol + host + "/api/v3/projects/" + project_id + "/notices?key=" + key;
