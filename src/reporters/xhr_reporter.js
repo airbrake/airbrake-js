21113d149583808eb26a4daf501df5ce2b628c45
Duncan Beevers <duncan@dweebd.com>
2013-07-16 11:28:54 -0700

    Extract ReportBuilder from Reporter

--- src/reporters/xhr_reporter.js
+++ src/reporters/xhr_reporter.js
@@ -1,57 +1,14 @@
-var merge = require("../util/merge");
+var ReportBuilder = require("../reporters/report_builder");
 
 function XhrReporter(url, environment_name, processor_name, custom_context_data, custom_environment_data, custom_session_data, custom_params_data) {
-
-  // Responsible for creating a payload consumable by the Airbrake v3 API
-  function generateOutputData(error_data) {
-    // `error_data` should be of the format
-    //   { type: String,
-    //     message: String,
-    //     backtrace: Array
-    //   }
-    //
-    // Each member of `error_data.backtrace` should be of the format
-    //   { file: String,
-    //     line: Number,
-    //     function: String
-    //   }
-
-    var notifier_data = {
-      name    : "Airbrake JS",
-      version : "<%= pkg.version %>+" + processor_name,
-      url     : "http://airbrake.io"
-    };
-
-    var context_data = merge(custom_context_data, {
-      language    : "JavaScript",
-      environment : environment_name
-    });
-
-    // Build the mandatory pieces of the output payload
-    var output_data = {
-      notifier : notifier_data,
-      context  : context_data,
-      errors   : [ error_data ]
-    };
-
-    // Add optional top-level keys to the output payload
-    if (custom_environment_data) { merge(output_data, { environment: custom_environment_data }); }
-    if (custom_session_data) { merge(output_data, { session: custom_session_data }); }
-    if (custom_params_data) { merge(output_data, { params: custom_params_data }); }
-
-    return output_data;
-  }
-
   this.report = function(error_data) {
-    var output_data = generateOutputData(error_data);
+    var output_data = ReportBuilder.build(environment_name, processor_name, custom_context_data, custom_environment_data, custom_session_data, custom_params_data, error_data);
 
     var request = new global.XMLHttpRequest();
     request.open('POST', url, true);
     request.setRequestHeader("Content-Type", "application/json");
     request.send(JSON.stringify(output_data));
   };
-
-  this.generateOutputData = generateOutputData;
 }
 
 module.exports = XhrReporter;
---
+++ test/unit/reporters/report_builder_test.coffee
@@ -0,0 +1,57 @@
+expect = require("chai").expect
+
+ReportBuilder = require("../../../src/reporters/report_builder")
+
+describe "ReportBuilder", ->
+  describe "build", ->
+    result = null
+    beforeEach ->
+      result = ReportBuilder.build("[environment_name]", "[processor_name]")
+
+    it "has `notifier`", ->
+      expect(result.notifier).to.deep.equal(
+        name: "Airbrake JS",
+        version: "<%= pkg.version %>+[processor_name]",
+        url: "http://airbrake.io"
+      )
+
+    it "has `context`", ->
+      expect(result.context).to.deep.equal(
+        language: "JavaScript",
+        environment: "[environment_name]"
+      )
+
+    it "merges custom context", ->
+      result = ReportBuilder.build(null, null, "CONTEXT_KEY": "CONTEXT_VAL")
+      expect(result.context.CONTEXT_KEY).to.equal("CONTEXT_VAL")
+
+    it "has no `environment`", ->
+      expect(result).not.to.have.property("environment")
+
+    it "merges custom environment", ->
+      result = ReportBuilder.build(null, null, null, "ENV_KEY": "ENV_VAL")
+      expect(result.environment).to.deep.equal("ENV_KEY": "ENV_VAL")
+
+    it "has no `session`", ->
+      expect(result).not.to.have.property("session")
+
+    it "merges custom session", ->
+      result = ReportBuilder.build(null, null, null, null, "SESS_KEY": "SESS_VAL")
+      expect(result.session).to.deep.equal("SESS_KEY": "SESS_VAL")
+
+    it "has no `params`", ->
+      expect(result).not.to.have.property("params")
+
+    it "merges custom params", ->
+      result = ReportBuilder.build(null, null, null, null, null, "PARAM_KEY": "PARAM_VAL")
+      expect(result.params).to.deep.equal("PARAM_KEY": "PARAM_VAL")
+
+    it "has `errors`", ->
+      result = ReportBuilder.build(null, null, null, null, null, null, type: "ERR_DATA_TYPE", message: "ERR_DATA_MESSAGE", backtrace: [])
+      errors = result.errors
+      expect(errors.length).to.equal(1)
+      expect(errors[0]).to.deep.equal(
+        type: "ERR_DATA_TYPE"
+        message: "ERR_DATA_MESSAGE"
+        backtrace: []
+      )
