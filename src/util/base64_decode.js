16b73b98a4c7ace14b48d258bbc70ed59d11143a
Duncan Beevers <duncan@dweebd.com>
2013-07-25 11:47:44 -0700

    Swap out base64 decoder

--- src/util/base64_decode.js
+++ src/util/base64_decode.js
@@ -1,43 +1,58 @@
-var lookup,
-    fromCharCode = String.fromCharCode;
+/*
+Copyright Vassilis Petroulias [DRDigit]
 
-function buildLookup() {
-  var i, z, k, v = [],
-      n = [ [ 65, 91 ], [ 97, 123 ], [ 48, 58 ], [ 43, 44 ], [ 47, 48 ] ];
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-  lookup = {};
+       http://www.apache.org/licenses/LICENSE-2.0
 
-  for (z in n) {
-    for(i = n[z][0]; i < n[z][1]; i++) {
-      v.push(fromCharCode(i));
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+var B64 = {
+    alphabet: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=',
+    lookup: null,
+    decode: function (s) {
+        var buffer = B64.fromUtf8(s),
+            position = 0,
+            len = buffer.length;
+        result = '';
+        while (position < len) {
+            if (buffer[position] < 128) result += String.fromCharCode(buffer[position++]);
+            else if (buffer[position] > 191 && buffer[position] < 224) result += String.fromCharCode(((buffer[position++] & 31) << 6) | (buffer[position++] & 63));
+            else result += String.fromCharCode(((buffer[position++] & 15) << 12) | ((buffer[position++] & 63) << 6) | (buffer[position++] & 63));
+        }
+        return result;
+    },
+    fromUtf8: function (s) {
+        var position = -1,
+            len, buffer = [],
+            enc = [, , , ];
+        if (!B64.lookup) {
+            len = B64.alphabet.length;
+            B64.lookup = {};
+            while (++position < len)
+            B64.lookup[B64.alphabet[position]] = position;
+            position = -1;
+        }
+        len = s.length;
+        while (position < len) {
+            enc[0] = B64.lookup[s.charAt(++position)];
+            enc[1] = B64.lookup[s.charAt(++position)];
+            buffer.push((enc[0] << 2) | (enc[1] >> 4));
+            enc[2] = B64.lookup[s.charAt(++position)];
+            if (enc[2] == 64) break;
+            buffer.push(((enc[1] & 15) << 4) | (enc[2] >> 2));
+            enc[3] = B64.lookup[s.charAt(++position)];
+            if (enc[3] == 64) break;
+            buffer.push(((enc[2] & 3) << 6) | enc[3]);
+        }
+        return buffer;
     }
-  }
-  for(i = 0; i < 64; i++) {
-    lookup[v[i]] = i;
-  }
-}
+};
 
-function decode_base64(s) {
-  if (!lookup) { buildLookup(); }
-
-  var result = "",
-      i, x, b, c, l, o;
-
-  for (i = 0; i < s.length; i += 72) {
-    b = 0;
-    l = 0;
-    o = s.substring(i, i + 72);
-
-    for (x = 0; x < o.length; x++) {
-      c = lookup[ o.charAt(x) ];
-      b = (b << 6) + c;
-      l += 6;
-      while (l >= 8) {
-        result += fromCharCode((b >>> (l -= 8)) & 0xff);
-      }
-    }
-  }
-  return result;
-}
-
-module.exports = decode_base64;
+module.exports = B64;
