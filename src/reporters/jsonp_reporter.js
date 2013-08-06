42ed4314957399fa3e1abc0fd2bb0c4d1bdeb070
Duncan Beevers <duncan@dweebd.com>
2013-08-06 16:47:28 -0700

    Consume JSON payload as JSONP

--- src/reporters/jsonp_reporter.js
+++ src/reporters/jsonp_reporter.js
@@ -1,5 +1,7 @@
 var ReportBuilder = require("../reporters/report_builder");
 
+var cb_count = 0;
+
 function JsonpReporter(project_id, project_key, environment_name, processor_name, custom_context_data, custom_environment_data, custom_session_data, custom_params_data) {
   this.report = function(error_data) {
     var output_data = ReportBuilder.build(environment_name, processor_name, custom_context_data, custom_environment_data, custom_session_data, custom_params_data, error_data),
@@ -7,7 +9,14 @@ function JsonpReporter(project_id, project_key, environment_name, processor_name
         head        = document.getElementsByTagName("head")[0],
         script_tag  = document.createElement("script"),
         body        = JSON.stringify(output_data),
-        url         = "https://api.airbrake.io/api/v3/projects/" + project_id + "/create-notice?key=" + project_key + "&body=" + encodeURIComponent(body);
+        cb_name     = "airbrake_cb_" + cb_count,
+        url         = "https://api.airbrake.io/api/v3/projects/" + project_id + "/create-notice?key=" + project_key + "&cb=" + cb_name + "&body=" + encodeURIComponent(body);
+
+
+    // Attach an anonymous function to the global namespace to consume the callback.
+    // This pevents syntax errors from trying to directly execute the JSON response.
+    global[cb_name] = function() { delete global[cb_name]; };
+    cb_count += 1;
 
     function removeTag() { head.removeChild(script_tag); }
 
