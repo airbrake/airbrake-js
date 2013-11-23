170fe3aee79f4d1eae314a357cd6dd68f20b08f1
Duncan Beevers <duncan@dweebd.com>
2013-11-22 18:25:52 -0600

    XhrReporter constructor takes no `name` parameter

--- src/reporters/xhr_reporter.js
+++ src/reporters/xhr_reporter.js
@@ -1,6 +1,6 @@
 var ReportBuilder = require("../reporters/report_builder");
 
-function XhrReporter(project_id, project_key, name, processor_name) {
+function XhrReporter(project_id, project_key, processor_name) {
   this.report = function(error_data, options) {
     var output_data = ReportBuilder.build(processor_name, error_data, options),
         url         = "https://api.airbrake.io/api/v3/projects/" + project_id + "/notices?key=" + project_key,
