658d5df460b5c36b4461b1d7c72103b5a228d307
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2014-03-12 10:35:43 +0200

    Use latest version in the shim.

--- examples/airbrake-shim.js
+++ examples/airbrake-shim.js
@@ -28,7 +28,7 @@ window.onerror = function(message, file, line) {
 var loadAirbrakeNotifier = function() {
   var script = document.createElement('script'),
       sibling = document.getElementsByTagName('script')[0];
-  script.src = 'https://ssljscdn.airbrake.io/0.3.2/airbrake.min.js';
+  script.src = 'https://ssljscdn.airbrake.io/0.3.3/airbrake.min.js';
   script.async = true;
   sibling.parentNode.insertBefore(script, sibling);
 }
