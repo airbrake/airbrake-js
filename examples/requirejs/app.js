35e8d6f72408645f6cac90fa907e2a33a55879c5
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2015-03-23 14:59:35 +0200

    Add requirejs example.

---
+++ examples/requirejs/app.js
@@ -0,0 +1,19 @@
+require.config({
+  paths: {
+    airbrakeJs: 'node_modules/airbrake-js/dist'
+  }
+});
+
+require(['airbrakeJs/client', 'airbrakeJs/instrumentation/jquery'],
+        function (AirbrakeClient, instrumentJQuery) {
+  airbrake = new AirbrakeClient({projectId: 1, projectKey: 'abc'})
+  if (window.jQuery) {
+    instrumentJQuery(airbrake, jQuery)
+  }
+
+  try {
+    throw new Error('hello from airbrake-js')
+  } catch (err) {
+    airbrake.push(err)
+  }
+});
