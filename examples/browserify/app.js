7b1d67333de484b89544b7dac9d0e7ca5186e136
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2017-02-13 12:34:24 +0200

    Update examples.

--- examples/browserify/app.js
+++ examples/browserify/app.js
@@ -1,16 +1,20 @@
-var AirbrakeClient = require('airbrake-js'),
-    instrumentJQuery = require('airbrake-js/dist/instrumentation/jquery.js');
+function start() {
+  var AirbrakeClient = require('airbrake-js'),
+      instrumentJQuery = require('airbrake-js/dist/instrumentation/jquery.js');
 
-var airbrake = new AirbrakeClient({projectId: 1, projectKey: 'FIXME'});
-if (window.jQuery) {
-  instrumentJQuery(airbrake, jQuery);
+  var airbrake = new AirbrakeClient({projectId: 1, projectKey: 'FIXME'});
+  if (window.jQuery) {
+    instrumentJQuery(airbrake, jQuery);
+  }
+
+  try {
+    throw new Error('hello from airbrake-js');
+  } catch (err) {
+    promise = airbrake.notify(err);
+    promise.then(function(notice) {
+      console.log("notice id", notice.id);
+    });
+  }
 }
 
-try {
-  throw new Error('hello from airbrake-js');
-} catch (err) {
-  promise = airbrake.notify(err);
-  promise.then(function(notice) {
-    console.log("notice id", notice.id);
-  });
-}
+start();
