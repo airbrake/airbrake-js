be0fdda9b93e0adfef6428d8638f337f144284c1
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2017-11-29 12:01:26 +0200

    Improve Express.js example

--- examples/express/app.js
+++ examples/express/app.js
@@ -1,18 +1,20 @@
 var express = require('express');
-var app = express();
 var AirbrakeClient = require('airbrake-js');
 var makeErrorHandler = require('airbrake-js/dist/instrumentation/express');
 
-var airbrake = new AirbrakeClient({
-  projectId: 1,
-  projectKey: 'FIXME'
-});
+var app = express();
 
 app.get('/', function hello (req, res) {
   throw new Error('hello from Express');
   res.send('Hello World!');
 })
 
+var airbrake = new AirbrakeClient({
+  projectId: 1,
+  projectKey: 'FIXME',
+});
+// Error handler middleware should be the last one.
+// See http://expressjs.com/en/guide/error-handling.html
 app.use(makeErrorHandler(airbrake));
 
 app.listen(3000, function () {
