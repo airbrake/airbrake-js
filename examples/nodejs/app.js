9258b2ee3a0f9de821d1fe0eb99900a7ea1b651d
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2018-02-06 15:43:56 +0200

    examples/nodejs: serve (not) favicon

--- examples/nodejs/app.js
+++ examples/nodejs/app.js
@@ -2,6 +2,12 @@ var http = require('http');
 var AirbrakeClient = require('airbrake-js');
 
 http.createServer(function (req, res) {
+  if (req.url === '/favicon.ico') {
+    res.writeHead(404);
+    res.end();
+    return;
+  }
+
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
   throw new Error('I am an uncaught exception');
