14bb9c9c8fc9b9a2ff00551fbe19852dcac5e7b0
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2018-07-23 11:54:39 +0300

    v1.4.3

--- dist/instrumentation/express.js
+++ dist/instrumentation/express.js
@@ -1,4 +1,4 @@
-/*! airbrake-js v1.4.2 */
+/*! airbrake-js v1.4.3 */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
