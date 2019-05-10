64ca970df8cf98f38763be219fbd225dbda567c7
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2019-05-10 15:01:27 +0300

    Update comment

--- examples/nodejs/app.js
+++ examples/nodejs/app.js
@@ -1,22 +1,24 @@
 var http = require('http');
-// make sure `request` package is installed before using airbrake-js
+// make sure `cross-fetch` package is installed before using airbrake-js
 var AirbrakeClient = require('airbrake-js');
 
-http.createServer(function (req, res) {
-  if (req.url === '/favicon.ico') {
-    res.writeHead(404);
-    res.end();
-    return;
-  }
+http
+  .createServer(function(req, res) {
+    if (req.url === '/favicon.ico') {
+      res.writeHead(404);
+      res.end();
+      return;
+    }
 
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-  throw new Error('I am an uncaught exception');
-}).listen(8080);
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+    throw new Error('I am an uncaught exception');
+  })
+  .listen(8080);
 
 console.log('Server running on port 8080.');
 
 var airbrake = new AirbrakeClient({
   projectId: 1,
-  projectKey: 'FIXME'
+  projectKey: 'FIXME',
 });
