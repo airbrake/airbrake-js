ff917a353832f67475a4d5943ca686021e572c66
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2019-07-29 13:22:41 +0300

    Update express example

--- examples/express/app.js
+++ examples/express/app.js
@@ -1,30 +1,30 @@
-var express = require('express')
-var AirbrakeClient = require('airbrake-js')
-var airbrakeExpress = require('airbrake-js/dist/instrumentation/express')
+const express = require('express');
+const AirbrakeClient = require('airbrake-js');
+const airbrakeExpress = require('airbrake-js/dist/instrumentation/express');
 
-var app = express()
+const app = express();
 
-var airbrake = new AirbrakeClient({
-  projectId: 1,
-  projectKey: 'FIXME'
-})
+const airbrake = new AirbrakeClient({
+  projectId: 123,
+  projectKey: 'FIXME',
+});
 
 // This middleware should be used before any routes are defined.
-app.use(airbrakeExpress.makeMiddleware(airbrake))
+app.use(airbrakeExpress.makeMiddleware(airbrake));
 
 app.get('/', function hello(req, res) {
-  throw new Error('hello from Express')
-  res.send('Hello World!')
-})
+  throw new Error('hello from Express');
+  res.send('Hello World!');
+});
 
 app.get('/hello/:name', function hello(req, res) {
-  res.send(`Hello ${req.params.name}`)
-})
+  res.send(`Hello ${req.params.name}`);
+});
 
 // Error handler middleware should be the last one.
 // See http://expressjs.com/en/guide/error-handling.html
-app.use(airbrakeExpress.makeErrorHandler(airbrake))
+app.use(airbrakeExpress.makeErrorHandler(airbrake));
 
 app.listen(3000, function() {
-  console.log('Example app listening on port 3000!')
-})
+  console.log('Example app listening on port 3000!');
+});
