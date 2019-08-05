8d503bb3a3cac9e5c0507c317b60fd2259c5df36
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2019-08-05 15:55:32 +0300

    Add packages/node

---
+++ packages/node/src/filter/node.ts
@@ -0,0 +1,36 @@
+import Notice from '../notice';
+
+const os = require('os');
+
+export default function filter(notice: Notice): Notice {
+  notice.context.os = `${os.type()}/${os.release()}`;
+  notice.context.architecture = os.arch();
+  notice.context.hostname = os.hostname();
+
+  notice.params.os = {
+    homedir: os.homedir(),
+    uptime: os.uptime(),
+    freemem: os.freemem(),
+    totalmem: os.totalmem(),
+    loadavg: os.loadavg(),
+  };
+
+  notice.context.platform = process.platform;
+  if (!notice.context.rootDirectory) {
+    notice.context.rootDirectory = process.cwd();
+  }
+
+  notice.params.process = {
+    pid: process.pid,
+    cwd: process.cwd(),
+    execPath: process.execPath,
+    argv: process.argv,
+  };
+  ['uptime', 'cpuUsage', 'memoryUsage'].map((name) => {
+    if (process[name]) {
+      notice.params.process[name] = process[name]();
+    }
+  });
+
+  return notice;
+}
