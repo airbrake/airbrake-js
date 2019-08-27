2ce8b103ccfb3926bf682f11a457ef786689139e
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2019-08-27 14:16:17 +0300

    Restore types definition

---
+++ packages/browser/src/airbrake.d.ts
@@ -0,0 +1,39 @@
+declare module 'airbrake' {
+  export class Notifier {
+    constructor(opts?: any);
+    public addFilter(filter: Filter): void;
+    public notify(err: any): Promise<INotice>;
+    public wrap(fn: any): IFuncWrapper;
+    public call(fn: any, ...args: any[]): any;
+    public onerror(): void;
+  }
+
+  interface IFuncWrapper {
+    (): any;
+    inner: () => any;
+  }
+
+  type Filter = (notice: INotice) => INotice | null;
+
+  interface INotice {
+    id: string;
+    errors: IAirbrakeError[];
+    context: any;
+    params: any;
+    session: any;
+    environment: any;
+  }
+
+  interface IAirbrakeFrame {
+    function: string;
+    file: string;
+    line: number;
+    column: number;
+  }
+
+  interface IAirbrakeError {
+    type: string;
+    message: string;
+    backtrace: IAirbrakeFrame[];
+  }
+}
