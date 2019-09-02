7d75c6407e1a8b0f3b4730a7c5986ed5a48a7e78
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2019-09-02 13:08:07 +0300

    Use ts-jest

--- packages/browser/babel.config.js
+++ packages/browser/babel.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    '@babel/preset-typescript',
-  ],
-  plugins: [['transform-define', { VERSION: '1.0.0' }]],
+  presets: [['@babel/preset-env', { targets: { node: 'current' } }]],
 };
