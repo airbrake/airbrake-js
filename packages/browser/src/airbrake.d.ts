2423ef3f6c27ff2a952c9d3bdb01d235177c782e
Steve Gray <swgray11@gmail.com>
2019-09-17 11:39:30 -0500

    Update type definitions

--- packages/browser/src/airbrake.d.ts
+++ packages/browser/src/airbrake.d.ts
@@ -1,39 +1,37 @@
-declare module 'airbrake' {
-  export class Notifier {
-    constructor(opts?: any);
-    public addFilter(filter: Filter): void;
-    public notify(err: any): Promise<INotice>;
-    public wrap(fn: any): IFuncWrapper;
-    public call(fn: any, ...args: any[]): any;
-    public onerror(): void;
-  }
+export declare class Notifier {
+  constructor(opts?: any);
+  public addFilter(filter: Filter): void;
+  public notify(err: any): Promise<INotice>;
+  public wrap(fn: any): IFuncWrapper;
+  public call(fn: any, ...args: any[]): any;
+  public onerror(): void;
+}
 
-  interface IFuncWrapper {
-    (): any;
-    inner: () => any;
-  }
+interface IFuncWrapper {
+  (): any;
+  inner: () => any;
+}
 
-  type Filter = (notice: INotice) => INotice | null;
+type Filter = (notice: INotice) => INotice | null;
 
-  interface INotice {
-    id: string;
-    errors: IAirbrakeError[];
-    context: any;
-    params: any;
-    session: any;
-    environment: any;
-  }
+interface INotice {
+  id: string;
+  errors: IAirbrakeError[];
+  context: any;
+  params: any;
+  session: any;
+  environment: any;
+}
 
-  interface IAirbrakeFrame {
-    function: string;
-    file: string;
-    line: number;
-    column: number;
-  }
+interface IAirbrakeFrame {
+  function: string;
+  file: string;
+  line: number;
+  column: number;
+}
 
-  interface IAirbrakeError {
-    type: string;
-    message: string;
-    backtrace: IAirbrakeFrame[];
-  }
+interface IAirbrakeError {
+  type: string;
+  message: string;
+  backtrace: IAirbrakeFrame[];
 }
