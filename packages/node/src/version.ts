18d95952335147fd10f2168974f388e5f008deb7
Kyrylo Silin <silin@kyrylo.org>
2021-02-19 16:33:48 +0200

    Release v2.1.0

--- packages/node/src/version.ts
+++ packages/node/src/version.ts
@@ -1,4 +1,4 @@
 export const NOTIFIER_NAME = 'airbrake-js/node';
-export const NOTIFIER_VERSION = '2.0.0';
+export const NOTIFIER_VERSION = '2.1.0';
 export const NOTIFIER_URL =
   'https://github.com/airbrake/airbrake-js/tree/master/packages/node';
